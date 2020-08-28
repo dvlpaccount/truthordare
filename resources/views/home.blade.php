91a1125b98269b34bd4be8016afa122669f4ad2d
Nawfal ? <60211944+NawfalHadi@users.noreply.github.com>
2020-08-28 16:47:10 +0700

    not from issues
    designing home.blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('source.home-source')
+
+@section('content')
+
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title m-b-md">
+            <img src="{{ asset('imgD/avatar.png') }}" alt=""> <br>
+                Welcome, NawlHadaif
+            </div>
+
+            <div class="links">
+            <a href="">Show Profie</a>
+            <a href="">Edit Profile</a>
+            <a href="">Logout <i class="fas fa-sign-out-alt"></i></a> 
+            </div>
+        </div>
+    </div>
+
+@endsection
+        
+   
+
---
+++ resources/views/source/home-source.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
+
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
